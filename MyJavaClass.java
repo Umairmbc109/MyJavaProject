5aa978e01ef261b88e02a04a439ef08ecc6b4000
Umairmbc109 <umairmbc109@gmail.com>
2024-02-15 11:12:12 +0500

    My Second Commit
    i am on the way

--- MyJavaClass.java
+++ MyJavaClass.java
@@ -1,5 +1,5 @@
 public class MyJavaClass {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello, GitHub! I am on the way");
     }
 }
